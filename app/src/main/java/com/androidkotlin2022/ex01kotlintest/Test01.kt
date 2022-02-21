0499f1e8ef365850ec813e87ddb17eded24b48d5
AndroidKotlin2022 <2022mrhi@gmail.com>
2022-02-21 16:08:13 +0900

    create Test01.kt

---
+++ app/src/main/java/com/androidkotlin2022/ex01kotlintest/Test01.kt
@@ -0,0 +1,26 @@
+package com.androidkotlin2022.ex01kotlintest
+
+// 실습내용
+// 1. 화면출럭
+
+fun main(){
+
+    /* .kt 코틀린 파일만 별도로 실행하려면 마우스 우클릭으로 해당파일 run 메뉴 실행 - 결과는 하단 [run]탭에서 실행 */
+
+    // 1. 화면출력
+    print("Hello")
+    print(10)
+    print(3.14)
+    //자동으로 줄바꿈 되지 않음.
+
+    println()
+    println("Nice")
+    println(100)
+    println(true)
+
+    // 변수명을 전달하면 변수안에 값이 출력됨
+    var a:Int=10
+    println( a )
+
+
+}
