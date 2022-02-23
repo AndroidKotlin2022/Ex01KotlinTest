9104c8d9c5a91a3261d7a2cacef26010e578b034
AndroidKotlin2022 <2022mrhi@gmail.com>
2022-02-23 12:22:34 +0900

    modify Test01.kt add 2. 자료형과 변수

--- app/src/main/java/com/androidkotlin2022/ex01kotlintest/Test01.kt
+++ app/src/main/java/com/androidkotlin2022/ex01kotlintest/Test01.kt
@@ -24,5 +24,23 @@ fun main(){
     var b:String="Hello"
     println(b)
 
+    //2. 자료형과 변수
+    // * 코틀린 자료형의 종류
+    // Boolean, Byte, Char, Short, Int, Long, Float, Double . . .
+    // String, Any, Unit ...  그외 Kotlin APIs, Java APIs
+
+    // * 변수의 2가지 종류 : var, val
+    // 2.1 var [ 값변경이 가능한 변수 ]
+    var num:Int = 10
+    println(num)
+
+    var num2:Double = 3.14
+    println(num2)
+
+    // 2.2 val [ 값 변경이 불가능한 변수 - 읽기전용 변수 ]  ## 상수와는 조금 다른 개념임 - 상수는 const 키워드 사용 ##
+
+
+
+
 
 }
